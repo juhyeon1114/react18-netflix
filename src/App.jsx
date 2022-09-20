3e3c37ad4ac61351125a8396bd8eb9694815d57d
juhyeon1114 <juhyeon1114@gmail.com>
2022-09-20 15:24:41 +0900

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { requests } from './api/requests'
 import Banner from './components/Banner'
+import Footer from './components/Footer'
 import Nav from './components/Nav'
 import Row from './components/Row'
 
@@ -9,10 +10,15 @@ const App = () => {
   return <div className='app'>
     <Nav></Nav>
     <Banner></Banner>
+    
+    {/* Rows */}
     <Row title="Netflix originals" id="NO" fetchUrl={requests.fetchNetflixOriginals} isLargeRow></Row>
     <Row title="Trending now" id="TN" fetchUrl={requests.fetchTrending}></Row>
     <Row title="Top rated" id="TR" fetchUrl={requests.fetchTopRated}></Row>
     <Row title="Action movies" id="AM" fetchUrl={requests.fetchActionMovies}></Row>
+    {/* /Rows */}
+    
+    <Footer></Footer>
   </div>
 }
 
